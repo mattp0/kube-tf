54f9b6d4aea781b2a8e50503314ab1d62a6bbda7
Matt Perry <mperry37@alaska.edu>
2022-01-13 13:59:04 -0900

    feat: add locals var to pass into modules

---
+++ locals.tf
@@ -0,0 +1,11 @@
+locals {
+  # Generic Project Vars
+  gcp_project    = "matt-test-2021"
+  gcp_region     = "us-west1"
+  gcp_zone       = "us-west1-c"
+  # GKE specific Vars
+  gke_num_nodes  = 2
+  machine_type   = "n2d-standard-2"
+  vpc            = "${local.gcp_project}-vpc"
+  subnet         = "${local.gcp_project}-subnet"
+}
